5433f219051c57f08fe7f5662bc6e36f49b509e7
ethanbaker22 <ethan.baker1999@gmail.com>
2021-02-02 16:14:18 +0000

    1st Block Stable State
    Added the purple shape and allowed rotation. Created boundaries for the shapes and added downwards velocity

---
+++ Assets/Tertis.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tertis : MonoBehaviour
+{
+    private float prevTime;
+    [SerializeField]
+    public float fallTime;
+
+    // Width & Height of the game area
+    public static double width = 10, height = 16;
+
+    public Vector3 rotation;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+            if (!IsValidMove())
+            {
+                transform.position -= new Vector3(-1, 0, 0);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += new Vector3(1, 0, 0);
+            if (!IsValidMove())
+            {
+                transform.position -= new Vector3(1, 0, 0);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        {
+            transform.RotateAround(transform.TransformPoint(rotation), new Vector3(0,0,1), 90);
+            if (!IsValidMove())
+            {
+                transform.RotateAround(transform.TransformPoint(rotation), new Vector3(0,0,1), -90);
+            }
+        }
+        
+        if (Time.time - prevTime > ((Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) ? fallTime / 10 : fallTime))
+        {
+            transform.position += new Vector3(0, -1, 0);
+            if (!IsValidMove())
+            {
+                transform.position -= new Vector3(0, -1, 0);
+            }
+            prevTime = Time.time;
+        }
+    }
+
+    bool IsValidMove()
+    {
+        foreach (Transform children in transform)
+        {
+            int x = Mathf.RoundToInt(children.transform.position.x);
+            int y = Mathf.RoundToInt(children.transform.position.y);
+        
+            if (x < 0 || x >= width || y < 0  || y >= height)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
